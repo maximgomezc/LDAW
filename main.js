017a404adc03b717f70f59864aca294c43263825
Maximo Gomez Campello <maximogomezcampello@gmail.com>
2026-08-20 19:51:12 -0300

    arreglo usuarios

--- main.js
+++ main.js
@@ -1,11 +1,19 @@
 import publicacion from "./publicacion.js";
+import usuario from "./usuario.js";
 
 const arregloPublicaciones = [
     new publicacion("porotos", "frijoles", "tortuga"),
     new publicacion("manzanas", "naranjas", "sapo"),
     new publicacion("berenjena", "violeta", "suricata"),
-    new publicacion("kiwi", "peludo", "mono")
+    new publicacion("kiwi", "peludo", "mono"),
+    new publicacion("banana", "amarilla", "mosca")
 ];
+
+const arregloUsuarios= [
+    new usuario("luca","lucakisner@gmail.com"),
+    new usuario("max", "maximogomezcampello@gmail.com"), 
+    new usuario ("seba", "sebapulles@gmail.com")
+]
 
 const publicacionKiwi = arregloPublicaciones.find(p => p.titulo === "kiwi");
 if (publicacionKiwi) {
