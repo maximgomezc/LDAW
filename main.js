71990cef088ff03dd1c7fd0173e71ac38f3667bd
lucakisner <lucagkisner@gmail.com>
2026-08-20 19:36:38 -0300

    asd

--- main.js
+++ main.js
@@ -1,14 +1,17 @@
 import publicacion from "./publicacion.js";
 
-const arregloPublicaciones= [
+const arregloPublicaciones = [
     new publicacion("porotos", "frijoles", "tortuga"),
     new publicacion("manzanas", "naranjas", "sapo"),
     new publicacion("berenjena", "violeta", "suricata"),
     new publicacion("kiwi", "peludo", "mono")
-]
+];
 
-const encontrado= arregloPublicaciones.find(publicacion.titulo==="kiwi") => publicacion.activa=false
+const publicacionKiwi = arregloPublicaciones.find(p => p.titulo === "kiwi");
+if (publicacionKiwi) {
+    publicacionKiwi.activa = false;
+}
 
 arregloPublicaciones.forEach(p => {
-    console.log(mostrarResumen(this.titulo, this.autor), estaActiva(this.activa))
+    console.log(p.mostrarResumen(p.titulo, p.autor), p.estaActiva(p.activa));
 });
